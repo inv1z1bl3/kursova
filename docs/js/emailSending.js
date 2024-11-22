e30b868180e5c6fba3f5c910d30ef26d6bc820eb
d3nik <den123u@gmail.com>
2024-11-22 18:08:20 +0200

    Patch 6.1 + 6.1.1 (hotfix)
    – some fixes in a whole html structure;
    – fixes in svg sprites realisation;
    – added color changing and transition to hover actions;
    – created mixins.scss for future adaptive content;

--- docs/js/emailSending.js
+++ docs/js/emailSending.js
@@ -1,21 +1,24 @@
+(function() {
+  emailjs.init('dJyKkms3ahuWUx8Uu')
+})();
+
 const btn = document.querySelector('.email-submit-button');
 
 document.querySelector('.email-update-form')
  .addEventListener('submit', function(event) {
    event.preventDefault();
 
-   btn.value = 'Sending...';
+   btn.textContent = 'Sending...';
 
    const serviceID = 'default_service';
    const templateID = 'template_mlxg5nl';
 
    emailjs.sendForm(serviceID, templateID, this)
     .then(() => {
-      btn.value = 'Send Email';
+      btn.textContent = 'Subscribe';
       alert('Sent!');
-      location.reload();
     }, (err) => {
-      btn.value = 'Send Email';
+      btn.textContent = 'Subscribe';
       alert(JSON.stringify(err));
     });
 });
